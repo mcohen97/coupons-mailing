925780b759b3e0b32873d1b606437e7c9a608954
= <diegomarcel27@hotmail.com>
2019-11-14 15:09:04 -0300

    Se modifica el controller de prueba

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,6 @@
 class HomeController < ApplicationController
 
   def index
-    SendExpirationMailJob.set(wait: 30.seconds).perform_later({user_email: 'diegomarcel27@hotmail.com'})
+    SendExpirationMailJob.set(wait: 2.minutes).perform_later('diegomarcel27@hotmail.com')
   end
 end
