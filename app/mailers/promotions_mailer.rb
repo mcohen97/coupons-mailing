4e2bd67a0c1523d61272685d4cfac4d2dafdab85
= <diegomarcel27@hotmail.com>
2019-11-14 15:07:55 -0300

    Se agrega validacion en el mailer

--- app/mailers/promotions_mailer.rb
+++ app/mailers/promotions_mailer.rb
@@ -1,10 +1,14 @@
 class PromotionsMailer < ApplicationMailer
 
   def send_expiration_notification
+    puts 'SE MANDA MAIL'
     @date = params[:date]
     @user_email = params[:user_email]
     @url  = 'http://example.com/login'
     mail(to: @user_email, subject: 'Welcome to My Awesome Site')
+  rescue Net::SMTPFatalError => e
+    puts 'ERROR'
+    Rails.logger.error(e.message)
   end
 
 end
