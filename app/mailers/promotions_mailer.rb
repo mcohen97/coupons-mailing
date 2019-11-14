a7ae6359b8714972d13f021d664e6643811cae08
= <diegomarcel27@hotmail.com>
2019-11-14 12:10:23 -0300

    Se actualizan vistas de mail y se modifica el metodo del mailer para enviar notificiaciones

--- app/mailers/promotions_mailer.rb
+++ app/mailers/promotions_mailer.rb
@@ -1,2 +1,10 @@
 class PromotionsMailer < ApplicationMailer
+
+  def send_expiration_notification
+    @date = params[:date]
+    @user_email = params[:user_email]
+    @url  = 'http://example.com/login'
+    mail(to: @user_email, subject: 'Welcome to My Awesome Site')
+  end
+
 end
