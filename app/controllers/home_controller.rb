72f0e362fd7efa9c3c157a9ea913e310524a0201
= <diegomarcel27@hotmail.com>
2019-11-16 12:06:32 -0300

    Se agrega mensaje de respuesta al endpoint de prueba

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,5 +4,6 @@ class HomeController < ApplicationController
 
   def index
     SendExpirationMailJob.set(wait: 2.minutes).perform_later('diegomarcel27@hotmail.com')
+    render json: {message: 'Mail scheduled to be sent successfully!'}, status: :ok
   end
 end
