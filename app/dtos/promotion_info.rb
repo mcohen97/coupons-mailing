4956385fdf3b28e819598d4bd0890d48485130f8
= <diegomarcel27@hotmail.com>
2019-11-16 14:41:54 -0300

    Se arregla dto de informacion de promocion

--- app/dtos/promotion_info.rb
+++ app/dtos/promotion_info.rb
@@ -2,11 +2,11 @@ class PromotionInfo
   attr_reader :expiration_date, :id, :name, :organization_id
 
   def initialize(data)
-    @applicable = Date.parse(data['expiration_date'])
+    @expiration_date = Date.parse(data['expiration_date'])
     @id = data['promotion_id']
     @organization_id = data['organization_id']
-    @name = data['name']
-    @code = data['code']
+    @name = data['promotion_name']
+    @code = data['promotion_code']
   end
 
 end
