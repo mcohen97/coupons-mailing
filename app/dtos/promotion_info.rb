66860d57b8d8241d715fe56a24e814ac97e680da
= <diegomarcel27@hotmail.com>
2019-11-17 15:09:16 -0300

    Se modifica la fecha en el dto

--- app/dtos/promotion_info.rb
+++ app/dtos/promotion_info.rb
@@ -1,12 +1,20 @@
 class PromotionInfo
-  attr_reader :expiration_date, :id, :name, :organization_id
+  attr_reader :expiration_date, :id, :name, :organization_id, :code
 
   def initialize(data)
-    @expiration_date = Date.parse(data['expiration_date'])
+    @expiration_date = format_date(data['expiration_date'])
     @id = data['promotion_id']
     @organization_id = data['organization_id']
     @name = data['promotion_name']
     @code = data['promotion_code']
+  end
+private
+  def format_date(the_string)
+    string_elements = the_string.split /[-T:+]+/
+    string_elements.pop
+    date = Time.new *(string_elements.map(&:to_i))
+    puts date
+    return date
   end
 
 end
