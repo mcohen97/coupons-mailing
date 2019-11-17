cf786c0cef70d401e571794b58aa6dee4ea3a403
= <diegomarcel27@hotmail.com>
2019-11-17 14:53:14 -0300

    Se renombra worker y se agrega otro para promociones editadas

--- app/workers/updated_promotions_worker.rb
+++ app/workers/updated_promotions_worker.rb
@@ -1,7 +1,7 @@
-class ExpirablePromotionsWorker
+class UpdatedPromotionsWorker
   include Sneakers::Worker
   # env is set to nil since by default the actual queue name would have the env appended to the end
-  from_queue ENV['CREATED_PROMOTIONS_QUEUE'],
+  from_queue ENV['UPDATED_PROMOTIONS_QUEUE'],
   env: nil,
   durable: true
 
@@ -11,9 +11,10 @@ class ExpirablePromotionsWorker
   # which we can pass to service without
   # changes
   def work(raw_data)
+    puts 'RECIBO PROMOCION'
     raw_data = JSON.parse(raw_data)
     promotion_info = PromotionInfo.new(raw_data)
-    Services.expiration_emails_scheduler.schedule_expiration(promotion_info)
+    Services.expiration_emails_scheduler.reschedule_expiration(promotion_info)
     ack! # we need to let queue know that message was received
   end
 
