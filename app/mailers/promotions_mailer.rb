87d7b11ff8557f3f4693d77e084394f69f4bee0c
= <diegomarcel27@hotmail.com>
2019-11-17 15:15:58 -0300

    Se agregan mas tipos de mails y sus respectivas vistas

--- app/mailers/promotions_mailer.rb
+++ app/mailers/promotions_mailer.rb
@@ -1,13 +1,33 @@
 class PromotionsMailer < ApplicationMailer
 
-  def send_expiration_notification
-    puts 'SE MANDA MAIL'
+  def send_expiring_soon_notification
+    @when = (params[:date].to_date == Date.today) ? "today" : "tomorrow" 
+    set_common_attributes
+    send_mail("Promotion #{@promotion_code} will expire soon")
+  end
+
+  def send_expired_notification
+    set_common_attributes
+    send_mail("Promotion #{@promotion_code} has expired")
+  end
+
+  def update_expiration_warning
+    puts 'ACTUALIZAR EL VENCIMIENTO'
+    set_common_attributes
     @date = params[:date]
+    send_mail("Promotion #{@promotion_code}'s expiration date has been updated")
+  end
+
+private
+
+  def set_common_attributes
     @user_email = params[:user_email]
     @promotion_code  = params[:promotion_code]
     @promotion_name = params[:promotion_name]
-    
-    mail(to: @user_email, subject: 'Promotion of code ')
+  end
+
+  def send_mail(subject)
+    mail(to: @user_email, subject: subject)
   rescue Net::SMTPFatalError => e
     puts 'ERROR'
     Rails.logger.error(e.message)
