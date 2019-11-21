1c6aa182d8348a2d09285c4e31f16e0af11ba1f5
= <diegomarcel27@hotmail.com>
2019-11-21 11:07:23 -0300

    Se arregla bug de obtencion de datos del hash

--- app/dtos/user_invitation_info.rb
+++ app/dtos/user_invitation_info.rb
@@ -1,11 +1,12 @@
 class UserInvitationInfo
-  attr_reader :email_invited, :sender_name, :code, :organization_name
+  attr_reader :email_invited, :sender_name, :sender_surname, :code, :organization_name
 
   def initialize(data)
-    @invitation_info = data[:email_invited]
-    @sender_name = data[:sender_name]
-    @code = data[:invitation_code]
-    @organization_name = data[:organization_name]
+    @email_invited = data['email_invited']
+    @sender_name = data['sender_name']
+    @sender_surname = data['sender_surname']
+    @code = data['invitation_code']
+    @organization_name = data['organization_name']
   end
 
 end
