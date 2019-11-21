3714c2f4e273f1cafd9cfabcab348ac6c5e4fc82
= <diegomarcel27@hotmail.com>
2019-11-21 11:09:28 -0300

    Se modifica mailer de invitaciones

--- app/mailers/user_invitations_mailer.rb
+++ app/mailers/user_invitations_mailer.rb
@@ -1,11 +1,14 @@
 class UserInvitationsMailer < ApplicationMailer
 
   def send_invitation
+    puts params.inspect
     @email_invited = params[:email_invited]
     @sender_name = params[:sender_name]
+    @sender_surname = params[:sender_surname]
     @invitation_code = params[:invitation_code]
     @organization_name = params[:organization_name]
     @registration_url = "#{params[:registration_url]}/#{@invitation_code}"
+    puts "mail para #{@email_invited}"
     mail(to: @email_invited, subject: @sender_name + ' invited you to join Coupons')
   end
 
