58b04b00c88c8ae30b64cd7eb70a7cedba7cb0aa
= <diegomarcel27@hotmail.com>
2019-11-21 11:18:15 -0300

    Se modifica dockerfile y se agrega script de entrypoint

---
+++ entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+# Remove a potentially pre-existing server.pid for Rails.
+rm -f /application/tmp/pids/server.pid
+
+sidekiq & disown
+
+WORKERS=CreatedPromotionsWorker,UpdatedPromotionsWorker,UserInvitedWorker rake sneakers:run
+
+
+
+# Then exec the container's main process (what's set as CMD in the Dockerfile).
+#exec "$@"
